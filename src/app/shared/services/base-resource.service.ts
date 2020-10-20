0264639068feca168a59976eea55cab4235e501a
Fabiano <jeovajire2003@gmail.com>
2020-10-20 18:55:04 -0300

    add models and services

---
+++ src/app/shared/services/base-resource.service.ts
@@ -0,0 +1,5 @@
+import { BaseResourceModel } from '../models/base-resource.model';
+
+export abstract class BaseResourceService<T extends BaseResourceModel> {
+  //TODO implementation of whole protected methods.
+}
