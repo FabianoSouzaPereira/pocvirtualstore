f4a779ea443335c4d7ff739afbba6541550e112a
Fabiano <jeovajire2003@gmail.com>
2020-10-23 09:48:34 -0300

    Corrigindo conflitos base resource

--- src/app/shared/services/base-resource.service.ts
+++ src/app/shared/services/base-resource.service.ts
@@ -18,37 +18,16 @@ export abstract class BaseResourceService<T extends BaseResourceModel> {
   protected formBuilder: FormBuilder;
 
   constructor(
-    protected apiPath: string,
-    protected injector: Injector,
-    protected jsonDataToResourceFn: (jsonData: any) => T
+
   ) {
-    this.http = injector.get(HttpClient);
+
   }
 
-  getAll(): Observable<T[]> {
-    return this.http.get(this.apiPath).pipe(
-      map(this.jsonDataToResources.bind(this)),
-      catchError(this.handleError)
-    )
-  }
+
+
 
   // PROTECTED METHODS
 
-  protected jsonDataToResources(jsonData: any[]): T[] {
-    const resources: T[] = [];
-    jsonData.forEach(
-      element => resources.push(this.jsonDataToResourceFn(element))
-    );
-    return resources;
-  }
 
-  protected jsonDataToResource(jsonData: any): T {
-    return this.jsonDataToResourceFn(jsonData);
-  }
-
-  protected handleError(error: any): Observable<any> {
-    console.log("ERRO NA REQUISIÇÃO => ", error);
-    return throwError(error);
-  }
 
 }
